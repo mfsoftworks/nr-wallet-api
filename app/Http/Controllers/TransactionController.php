10008332690232aa67891868b217fcf577d70aa4
mfcodeworks <arranjamesfletcher2012@gmail.com>
2020-06-16 01:36:25 +0800

    .

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -35,7 +35,7 @@ class TransactionController extends Controller
             'currency' => $from->default_currency ?? env('DEFAULT_CURRENCY', 'aud'),
             'customer' => $customer->id ?? null,
             'metadata' => [
-                'user_id' => $from->id ?? null,
+                'user_id' => $from->id ?? "",
                 'for_user_id' => $request->for_user_id
             ],
             'payment_method_types' => [
